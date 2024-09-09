3cc1fd3887c85dee5b764b2a59274c0877b8ab6a
Infinity7915 <dustinmatt360@gmail.com>
2024-09-09 17:37:45 -0500

    Update Resources1.lua

--- Resources1.lua
+++ Resources1.lua
@@ -1,3 +1,5 @@
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Infinity7915/Project-Paragon/main/Libraries.lua"))()
+
 local _Resources = { -- This table is to load in 'UNIVERSAL' needs
     general_resources = {
       ['antiAFK'] = function() 
