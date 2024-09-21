4666a21611ebe4eceaac4f6b44125181c8c7ca62
Infinity7915 <dustinmatt360@gmail.com>
2024-09-21 17:13:26 -0500

    Update Credits.lua

--- Credits.lua
+++ Credits.lua
@@ -1,6 +1,9 @@
 -- # Project Paragon
+-- Refrences:
+local player_display_name = player.DisplayName
+-- Storage:
 local _Credits_0 = function()
-  print("\n# Project Paragon \nCreator: 0_Paragon \nOwner: 0_Paragon \nUse: Personal use \nUtilizing V3 Libraries \nUser: " .. player.DisplayName)
+ print("\n# Project Paragon \nCreator: 0_Paragon \nOwner: 0_Paragon \nUse: Personal use \nUtilizing V3 Libraries \nUser: " .. player_display_name)
 end
-
+-- Callinging storage:
 _Credits_0()
