fec9dcc873bc13c218f70249aef8b1018eaafef9
Infinity7915 <dustinmatt360@gmail.com>
2024-09-21 17:10:47 -0500

    Update Credits.lua

--- Credits.lua
+++ Credits.lua
@@ -1,6 +1,6 @@
 -- # Project Paragon
 local _Credits_0 = function()
-  print("\n# Project Paragon \nCreator: 0_Paragon \nOwner: 0_Paragon \nUse: Personal use \nUtilizing V3 Libraries")
+  print("\n# Project Paragon \nCreator: 0_Paragon \nOwner: 0_Paragon \nUse: Personal use \nUtilizing V3 Libraries \nUser: " .. player.DisplayName)
 end
 
 _Credits_0()
